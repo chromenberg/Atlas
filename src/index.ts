baf45cbec47bf03cccd88ef966cc8ff72e592fa2
Raine <kalelmcevoy@hotmail.com>
2026-04-21 12:59:02 +0100

    fix some stuff

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { Atlas } from "./lib/AtlasClient.js";
-new Atlas()
+import { AtlasClient } from "./lib/AtlasClient.js";
+new AtlasClient()
 import { a } from "./lib/modules/pooling/Pool.js";
 a()
 // KEYSPACE HAS BEEN CREATED "ATLAS"
