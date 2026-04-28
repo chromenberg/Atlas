c091cad124929ddb6de46939eee08b1f7150a6e6
Raine <kalelmcevoy@hotmail.com>
2026-04-28 03:53:03 +0100

    update index.js for testing

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { Atlas } from "./lib/AtlasManager.js";
-
+import "./lib/modules/snowflake/Snowflake.js";
+// @ts-ignore
+console.log(SnowflakeGenerator(1,10,12,8597346).GenerateID().toString())
 // KEYSPACE HAS BEEN CREATED "ATLAS"
 // AtlasClient.init()
