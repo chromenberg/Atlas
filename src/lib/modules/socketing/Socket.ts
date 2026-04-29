0fb7673c311401bbc8f51421e627eb993d2f6f68
Raine <kalelmcevoy@hotmail.com>
2026-04-29 03:13:07 +0100

    unix socketing will be used to connect lagrange to atlas

--- src/lib/modules/socketing/Socket.ts
+++ src/lib/modules/socketing/Socket.ts
@@ -1,15 +1,19 @@
-import { createSocket } from "dgram";
-import { Sockets } from "./Routes.js";
-import { Logger, LogLevel } from "../../../../../Logging/dist/Logger.js";
-export function connectAtlas() {
-    const sock = createSocket({
-        type: "udp4",
-    });
-    sock.bind();
+import { createServer } from "net";
+import { Logger, LogLevel } from "../../../../../Common/Logging/dist/Logger.js";
+import { unlinkSync } from "fs";
 
+export function connectAtlas() {
+    const sock = createServer();
+    sock.listen("/tmp/WyvernSockets/Atlas", () => {
+        console.log("Connected");
+    })
     sock.on("message", (msg) => {
-        Logger.sendLog(LogLevel.Verbose, ["ATLAS", "Socketing", "Lagrange-Atlas"], msg.toJSON())
+        Logger.sendLog(LogLevel.Verbose, ["ATLAS", "Socketing", "Lagrange-Atlas"], msg.toJSON());
+    })
+
+    process.on("beforeExit", (code) => {
+        Logger.sendLog(LogLevel.Info, ["Process", "BeforeExit", "ATLAS"], "Disconnecting ATLAS Socket");
+        unlinkSync("/tmp/WyvernSockets/Atlas");
     })
 }
-
 
